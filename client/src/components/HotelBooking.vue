986597439294adad3694a8fec6e5df641e3f74f1
Dani <danikav1989@gmail.com>
2021-04-14 13:46:52 +0100

    client started

---
+++ client/src/components/HotelBooking.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='hotel_booking'>
+   <h2> {{hotel_booking.name}} </h2>
+   <p> {{hotel_booking.email}} </p>
+   <p> {{hotel_booking.status}} </p>
+
+   <button v-on:click='handleDelete(hotel_booking._id)'>Delete Booking</button>
+  </div>
+</template>
+
+<script>
+import {eventBus} from '@main.js'
+
+
+export default {
+    name: 'hotel_booking',
+    props: ['hotel_booking'],
+
+    methods: {
+        handleDelete(id){
+            eventBus.$emit('delete-hotel_booking', id);
+        }
+    }
+
+}
+</script>
+
+<style>
+
+
+</style>
